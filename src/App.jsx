2cb0c1bb01d1cbe19e32b143332068604822d88d
AnthonyCarmine <>
2024-03-13 14:25:38 -0500

    el primer commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './main'
+//import ComponenteTarjeta from './ComponenteTarjeta.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <body>
+
+      <nav id='navbar' class='w-full h-12  p-2 m-0'>
+          <p class="animacion">
+            <span class="icono material-symbols-outlined">
+              code
+              
+            </span>
+            <p class="titulo">Soy David Alejandro Osorio Sánchez</p>
+          </p>
+        <div>
+
+        </div>
+      </nav>
+      {/* <ComponenteTarjeta /> */}
+    </body>
+  )  
+}
+
+export default App
---
+++ src/componenteTarjeta.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ComponenteTarjeta (){
+
+    return(
+        <>
+            <h1 class='bg-gray-600'>hola mundo</h1>
+        </>
+    )
+        
+
+}
+export default ComponenteTarjeta;

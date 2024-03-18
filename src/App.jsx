e0c9ac5e7b389c401ffcab9fe5fb5d7fed57f3ff
David Osorio <daalosx@gmail.com>
2024-03-17 22:34:02 -0500

    main casi terminado

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,25 @@ function App() {
               <p class="titulo">Soy David Alejandro Osorio Sánchez</p>
             </p>
           <div>
-            {document.addEventListener("scroll", () => {
-              const borde = document.getElementById("borde");
-              if (window.pageYOffset < 50) {
-                borde.style = "opacity: 0; animation: normal desaparecer 1s ease;";
-              }
-              if(window.pageYOffset > 50) {
-                borde.style = "animation: normal aparecer 1s ease;";
-              }
-            })}
+            {
+              document.addEventListener("scroll", () => {
+                const borde = document.getElementById("borde");
+                if (window.pageYOffset < 30) {
+                  borde.style = "opacity: 0; animation: normal desaparecer 1s ease;";
+                }
+                if(window.pageYOffset > 30) {
+                  borde.style = "animation: normal aparecer 1s ease;";
+                }
+              })
+            }
+            {
+              /*
+              document.addEventListener('DOMContentLoaded', () => {
+                const borde = document.getElementById("borde");
+                borde.style = "opacity: 0"
+              })
+              */
+            }
           </div>
         </div>
         <div id='borde'></div>
--- src/Portada.jsx
+++ src/Portada.jsx
@@ -5,13 +5,21 @@ import './App.css'
 function Portada (){
     return(
         <div class="main">
-            
+            {/* targeta parte izquierda */}
             <Logos miAnimacion="JS" links="https://blog.desafiolatam.com/wp-content/uploads/2018/05/javascript-logo.png" /> 
-            <Logos miAnimacion="SQL" links="http://blog.desafiolatam.com/wp-content/uploads/2018/05/sql-logo.png" class="SQL"/>
-            <Logos miAnimacion="JAVA" links="http://blog.desafiolatam.com/wp-content/uploads/2018/05/java-logo.png" class="JAVA" />
+            <Logos miAnimacion="SQL" links="http://blog.desafiolatam.com/wp-content/uploads/2018/05/sql-logo.png" />
+            <Logos miAnimacion="JAVA" links="http://blog.desafiolatam.com/wp-content/uploads/2018/05/java-logo.png" />
             <Logos miAnimacion="REACT" links="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1024px-React_Logo_SVG.svg.png" /> 
-            <Logos miAnimacion="CSS" links="https://img-resize-cdn.joshmartin.ch/2000x0%2C363e360e8516e42e965658dab267b06b94b2899a751b80079cfce35583aa080c/https://joshmartin.ch/app/uploads/2017/10/css3.svg" class="SQL"/>
-            <Logos miAnimacion="HTML" links="https://res.cloudinary.com/thewebmaster/image/upload/c_scale,f_auto,q_auto:best,w_516/images/html/html5.png" class="JAVA" />
+            <Logos miAnimacion="CSS" links="https://img-resize-cdn.joshmartin.ch/2000x0%2C363e360e8516e42e965658dab267b06b94b2899a751b80079cfce35583aa080c/https://joshmartin.ch/app/uploads/2017/10/css3.svg" />
+            <Logos miAnimacion="HTML" links="https://res.cloudinary.com/thewebmaster/image/upload/c_scale,f_auto,q_auto:best,w_516/images/html/html5.png"/>
+            
+            {/* targeta parte derecha */}
+            <Logos miAnimacion="TAIL" links="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg" /> 
+            <Logos miAnimacion="BOOT" links="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png" />
+            <Logos miAnimacion="FLUTTER" links="https://web-strapi.mrmilu.com/uploads/flutter_logo_470e9f7491.png" />
+            <Logos miAnimacion="DART" links="https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png" /> 
+            <Logos miAnimacion="SPRING" links="https://www.armadilloamarillo.com/wp-content/uploads/spring-boot-ok.png" />
+            <Logos miAnimacion="PYTHON" links="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/220px-Python-logo-notext.svg.png" />
         </div>
     )
         

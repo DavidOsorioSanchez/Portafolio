8fcb49fe53fb9ba762e3cf52c05ea0e1bc1680f8
David Osorio <ecardona742@gmail.com>
2024-04-02 09:26:35 -0500

    corrigiendo responsive

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 function Footer() {
   return (
-    <footer className="!w-screen h-auto flex flex-col  min-h-24 bg-colorFooter border-solid border-t-2 border-colorBordePerfil gap-20 py-12">
+    <footer className="!w-full h-auto flex flex-col  min-h-24 bg-colorFooter border-solid border-t-2 border-colorBordePerfil gap-20 py-12">
       <p className="text-center text-5xl text-blancoEstrella">Formulario</p>
-      <form className="w-screen flex gap-5 flex-wrap justify-around">
+      <form className="w-full flex gap-5 flex-wrap justify-around">
         <input
           className="min-w-80 border-t-hidden border-r-hidden bg-transarente text-blancoEstrella border-solid border-b-4 border-l-2 border-colorBordePerfil rounded-tr-sm text-center"
           type="gmail"

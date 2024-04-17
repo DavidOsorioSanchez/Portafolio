9ca2045d73326311b131d6e803e5e24d0301dfe5
Mariana <marianabedoyac01@gmail.com>
2024-04-17 14:02:48 -0500

    probando las imagenes

--- src/componenteTarjeta.jsx
+++ src/componenteTarjeta.jsx
@@ -5,7 +5,7 @@ import "././css/App.css";
 function ComponenteTarjeta({ children, imagenTarjeta, titulo, subtitulo }) {
   return (
     <div
-      className={`w-2/5 min-w-80 max-h-max h-96 rounded-lg animate-bajada object-cover bg-no-repeat bg-cover bg-total bg-ArteyForma`}
+      className={`w-2/5 min-w-80 max-h-max h-96 rounded-lg animate-bajada object-cover bg-no-repeat bg-cover bg-total ${imagenTarjeta}`}
     >
       <div
         className={`w-full h-full flex flex-col transition ease-in-out duration-300 rounded-lg opacity-0 hover:opacity-100 hover:bg-BlancoTransparente`}
